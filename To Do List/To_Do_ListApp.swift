7f8b7fa87fda9c35d209565fceaaaa8410228403
Kelsey McIntyre <kelsrmcintyre@gmail.com>
2023-07-18 10:42:30 -0400

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Kelsey McIntyre on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
